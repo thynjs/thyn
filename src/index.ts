a7baad6bb42f70a3cb7b7c05284625b303645531
Travis Harmon <harmtrav@gmail.com>
2026-02-11 15:31:21 -0600

    Use conditional exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from "./core/signals.js";
 export * from "./core/element.js";
 export * from "./core/router.js";
-export { default } from "./plugin/index.js";
-export { transformSFC, compileSFC } from "./plugin/index.js";
---
+++ src/node.ts
@@ -0,0 +1,5 @@
+export * from "./core/signals.js";
+export * from "./core/element.js";
+export * from "./core/router.js";
+export { default } from "./plugin/index.js";
+export { transformSFC, compileSFC } from "./plugin/index.js";
